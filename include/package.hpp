d5b5cd675f11ea9c06301f8f4a8fe3fe244149df
KruchaBezKosci <pawel4040kowal@gmail.com>
2026-01-10 18:01:37 +0100

    move semantics are not yet implemented

--- include/package.hpp
+++ include/package.hpp
@@ -4,5 +4,20 @@
 
 #ifndef PACKAGE_HPP
 #define PACKAGE_HPP
+#include "types.hpp"
+#include <set>
+
+class Package {
+  public:
+    Package();
+    ~Package();
+    ElementID get_id() const { return id_; }
+
+   private:
+     ElementID id_;
+     static std::set<ElementID> assigned_IDs;
+     static std::set<ElementID> freed_IDs;
+};
+
 
 #endif //PACKAGE_HPP
--- src/package.cpp
+++ src/package.cpp
@@ -1,3 +1,33 @@
 //
 // Created by pawel on 04.01.2026.
 //
+#include "package.hpp"
+
+std::set<ElementID> Package::assigned_IDs;
+std::set<ElementID> Package::freed_IDs;
+
+
+Package::Package(){
+if(!freed_IDs.empty()){
+  id_= *freed_IDs.begin();
+  freed_IDs.erase(freed_IDs.begin());
+} else {
+  if (assigned_IDs.empty()) {
+    id_ = 1; //zwociu pierwsze id
+  } else {
+    // zwraca najwiekszy w secie
+    id_ = *assigned_IDs.rbegin() + 1;
+  }
+}
+//musi bcyc zajete
+assigned_IDs.insert(id_);
+};
+
+Package::~Package() {
+//gdy paczka nie została przenisiona gdzies
+    assigned_IDs.erase(id_);
+    freed_IDs.insert(id_);
+}
+
+
+
