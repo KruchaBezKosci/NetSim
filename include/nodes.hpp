fa065b69e1533df9cde8a53ead2eeb5ffa5ad5a9
Michał Kot <m45541724@gmail.com>
2026-01-11 22:10:54 +0100

    feat: add PackageSender class definition to nodes.hpp

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -8,6 +8,9 @@
 #include "types.hpp"
 #include "package.hpp"
 
+#include <optional>
+#include <vector>
+
 // Typy odbiorców wymagane do badania spójności sieci
 enum class ReceiverType {
     WORKER,
@@ -27,5 +30,34 @@ public:
     virtual ReceiverType get_receiver_type() const = 0;
 
     virtual ~IPackageReceiver() = default;
+
+
+class PackageSender {
+public:
+    PackageSender() = default;
+    PackageSender(PackageSender&&) = default;
+
+    // Metoda do dodawania odbiorcy (np. konkretnego robotnika do listy rampy)
+    void add_receiver(IPackageReceiver* receiver) { receivers_.push_back(receiver); }
+
+    // Metoda do sprawdzania, komu rampa może wysłać paczkę
+    const std::vector<IPackageReceiver*>& get_receivers() const { return receivers_; }
+
+    // Tu będzie się działo wysyłanie (logikę napiszemy w .cpp)
+    void send_package();
+
+    // Dostęp do bufora paczki (żeby sprawdzić, co rampa aktualnie "trzyma")
+    const std::optional<Package>& get_sending_buffer() const { return buffer_; }
+
+protected:
+    // Tu rampa umieszcza paczkę, zanim zostanie ona przekazana dalej
+    void push_package(Package&& p) { buffer_ = std::move(p); }
+
+    std::optional<Package> buffer_; // Bufor na jedną paczkę
+
+private:
+    std::vector<IPackageReceiver*> receivers_; // Lista potencjalnych odbiorców
+};
+
 };
 #endif //NETSIM_NODES_HPP
