2a694ff707b1f7a95d41a13ceb22d4b68bc4f824
LukiKnur <luk10052005@gmail.com>
2026-01-15 18:59:34 +0100

    Fixing mistakes.
    Źle umieściłem pliki, dlatego cofam zmiany.

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -43,29 +43,3 @@ void PackageSender::send_package() {
         }
     }
 }
-//
-// Lukasz 15.01.2026
-//
-void Worker::do_work(Time t) {
-   
-    if (!processing_buffer_.has_value() && !q_->empty()) {
-
-        processing_buffer_.emplace(q_->pop());
-
-        startTime_ = t;
-    }
-
-    if (processing_buffer_.has_value()) {
-
-        if (t - startTime_ + 1 >= pd_) {
-
-            push_package(std::move(processing_buffer_.value()));
-
-            processing_buffer_.reset();
-            startTime_ = 0;
-        }
-    }
-}
-//
-//
-//
