556de6215ac929ada57af0478433c573f9851852
LukaszChillGuy <luk10052005@gmail.com>
2026-01-16 12:34:39 +0100

    Update worker.hpp

--- include/worker.hpp
+++ include/worker.hpp
@@ -10,23 +10,17 @@
 #include <memory>
 
 
+
 class Worker : public PackageSender, public IPackageReceiver {
 public:
-
     Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
         : PackageSender(), id_(id), pd_(pd), q_(std::move(q)) {}
+
+    void do_work(Time t); 
 
     void receive_package(Package&& p) override { q_->push(std::move(p)); }
     ElementID get_id() const override { return id_; }
     ReceiverType get_receiver_type() const override { return ReceiverType::WORKER; }
-
-    void do_work(Time t);
-
-    TimeOffset get_processing_duration() const { return pd_; }
-    Time get_package_processing_start_time() const { return startTime_; }
-    IPackageQueue* get_queue() const { return q_.get(); }
-    
-    const std::optional<Package>& get_processing_buffer() const { return processing_buffer_; }
 
     IPackageStockpile::const_iterator begin() const override { return q_->begin(); }
     IPackageStockpile::const_iterator end() const override { return q_->end(); }
@@ -35,7 +29,7 @@ private:
     ElementID id_;
     TimeOffset pd_;
     std::unique_ptr<IPackageQueue> q_;
-    std::optional<Package> processing_buffer_; 
+    std::optional<Package> processing_buffer_;
     Time startTime_ = 0;
 };
 //
