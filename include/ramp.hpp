b459882e3144a87d8b6442f44d741d2d01e69914
LukaszChillGuy <luk10052005@gmail.com>
2026-01-17 13:42:07 +0100

    Create ramp.hpp

---
+++ include/ramp.hpp
@@ -0,0 +1,14 @@
+//
+// lukasz 17.01.26
+//
+
+class Ramp : public PackageSender {
+public:
+    Ramp(ElementID id, TimeOffset di) : PackageSender(), id(id), di(di) {}
+    void deliver_goods(Time t);
+    ElementID getid() const { return id; }
+    TimeOffset get_deliveryinterval() const { return di; }
+private:
+    ElementID id;
+    TimeOffset di;
+};
